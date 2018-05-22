40d9a13006fa9bfc814eec11da080586412cd6b3
Lauri Peltonen <laurippeltonen@gmail.com>
2018-05-22 21:57:23 +0300

    Basic crowdsale contracts ready, waiting for tests and customization

---
+++ contracts/Crowdsale/TimedCrowdsale.sol
@@ -0,0 +1,59 @@
+pragma solidity ^0.4.23;
+
+import "../SafeMath.sol";
+import "./WhitelistedCrowdsale.sol";
+
+
+/**
+ * @title TimedCrowdsale
+ * @dev Crowdsale accepting contributions only within a time frame.
+ */
+contract TimedCrowdsale is WhitelistedCrowdsale 
+{
+  using SafeMath for uint256;
+
+  uint256 public openingTime;
+  uint256 public closingTime;
+
+  /**
+   * @dev Reverts if not in crowdsale time range.
+   */
+  modifier onlyWhileOpen {
+    // solium-disable-next-line security/no-block-members
+    require(block.timestamp >= openingTime && block.timestamp <= closingTime, "Crowdsale is not open");
+    _;
+  }
+
+  /**
+   * @dev Constructor, takes crowdsale opening and closing times.
+   * @param _openingTime Crowdsale opening time
+   * @param _closingTime Crowdsale closing time
+   */
+  constructor(uint256 _openingTime, uint256 _closingTime) public {
+    // solium-disable-next-line security/no-block-members
+    require(_openingTime >= block.timestamp, "Opening time is not in the future");
+    require(_closingTime >= _openingTime, "Closing time is not after opening time");
+
+    openingTime = _openingTime;
+    closingTime = _closingTime;
+  }
+
+  /**
+   * @dev Checks whether the period in which the crowdsale is open has already elapsed.
+   * @return Whether crowdsale period has elapsed
+   */
+  function hasClosed() public view returns (bool) {
+    // solium-disable-next-line security/no-block-members
+    return block.timestamp > closingTime;
+  }
+
+  /**
+   * @dev Extend parent behavior requiring to be within contributing period
+   * @param _beneficiary Token purchaser
+   * @param _weiAmount Amount of wei contributed
+   */
+  function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal onlyWhileOpen {
+    super._preValidatePurchase(_beneficiary, _weiAmount);
+  }
+
+}
